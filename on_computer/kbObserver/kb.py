951e4b52a92dcbcf64927bf7ff05d25d1a5cef9c
gomjellie <gomjellie@naver.com>
2016-11-12 19:54:49 +0900

    observerPattern

---
+++ on_computer/kbObserver/kb.py
@@ -0,0 +1,52 @@
+from observable import Observable
+from observer import Observer
+import curses
+import time
+    
+class KeyBoard(object):
+    '''
+    on MAC 
+        68=>left, 66=>down, 67=>right, 65=>top
+
+    '''
+    def __init__(self):
+    	self.c = 0
+    	self.stdscr = curses.initscr()
+    	curses.noecho()
+    	curses.cbreak()
+    	self.stdscr.nodelay(1)
+    	
+    def get_ch(self):
+        self.c = self.stdscr.getch()
+        if self.c != -1:
+            if self.c == 68:
+                key_in = 'left'
+            elif self.c == 65:
+                key_in = 'top'
+            elif self.c == 67:
+                key_in = 'right'
+            elif self.c == 66:
+                key_in = 'down'
+            else:
+                key_in = None
+#            self.stdscr.addstr(' ' + str(self.c) + ' ' + key_in)
+#            self.stdscr.refresh()
+#            self.stdscr.move(0, 0)
+            return key_in
+        return None
+ 
+class Actor(Observer):
+    def __init__(self):
+    	self.c = 0
+    def update(self, *args, **kwargs):
+        if not kwargs.get('c') is None:
+            print('{0} '.format(kwargs.get('c')))
+
+
+if __name__ == '__main__':
+    observable = Observable()
+    keyBoard = KeyBoard()
+    actor = Actor()
+    observable.register(actor)
+    while 1:
+    	observable.update_observers(c = keyBoard.get_ch())
