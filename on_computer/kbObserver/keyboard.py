9a6ac170d9c614c71e4ce44d5e7a8c914e32d988
gomjellie <gomjellie@naver.com>
2016-11-12 21:06:19 +0900

    arrange codes, div class files

---
+++ on_computer/kbObserver/keyboard.py
@@ -0,0 +1,35 @@
+import curses
+import time
+    
+class KeyBoard(object):
+    '''
+    on MAC 
+        68=>left, 66=>down, 67=>right, 65=>top
+
+    '''
+    def __init__(self):
+    	self.c = 0
+    	self.stdscr = curses.initscr()
+    	curses.noecho()
+    	curses.cbreak()
+    	self.stdscr.nodelay(1)
+    	
+    def get_ch(self):
+        self.c = self.stdscr.getch()
+        if self.c != -1:
+            if self.c == 68:
+                key_in = 'left'
+            elif self.c == 65:
+                key_in = 'top'
+            elif self.c == 67:
+                key_in = 'right'
+            elif self.c == 66:
+                key_in = 'down'
+            else:
+                key_in = None
+#            self.stdscr.addstr(' ' + str(self.c) + ' ' + key_in)
+#            self.stdscr.refresh()
+#            self.stdscr.move(0, 0)
+            return key_in
+        return None
+ 
---
+++ on_computer/tae.py
@@ -0,0 +1,8 @@
+start_list = [5,3,1,2,4]
+square_list = []
+
+for start_list in start_list:
+    square_list.append(start_list**2)
+    print start_list
+square_list.sort()
+print square_list
